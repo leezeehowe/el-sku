a5eaa95e70e5b850a605c3832c1c28414066ad44
不要放香菜 <leezeehowe@gmail.com>
2020-10-31 13:26:40 +0800

    docs: 增加代码注释

--- src/columnType.js
+++ src/columnType.js
@@ -2,6 +2,9 @@ import ElSkuText from './components/el-sku-text.vue'
 import ElSkuNumber from './components/el-sku-number.vue'
 import {Switch} from 'element-ui'
 
+/**
+ * 数据类型到所使用的组件的映射
+ */
 const map = {
   text: ElSkuText,
   number: ElSkuNumber,
